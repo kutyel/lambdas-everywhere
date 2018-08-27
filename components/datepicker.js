7a48f4f117ac8f7f61dd1e54b66b6cedf40ee5aa
kutyel <flavio.corparios@ext.soprasteria.com>
2018-08-27 13:59:39 +0200

    Let there be light! 💡

---
+++ components/datepicker.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import DatePicker from "./DatePicker";
+import Input from "./Input";
+
+class PickerDemo extends Component {
+  state = {
+    focusedInput: null,
+    highlighted: { from: null, to: null },
+    value: { from: null, to: null }
+  };
+
+  componentWillMount() {
+    this.today = new Date();
+  }
+
+  handleChange = value => {
+    this.setState({ value });
+  };
+
+  handleHighlightedChange = highlighted => {
+    this.setState({ highlighted });
+  };
+
+  handleFocusedInputChange = focusedInput => {
+    this.setState({ focusedInput });
+  };
+
+  isDisabled = date => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    date.setHours(0, 0, 0, 0);
+    return date.getTime() < today.getTime();
+  };
+
+  handleStartInputFocus = () => {
+    this.setState({ focusedInput: "START_DATE" });
+  };
+
+  handleEndInputFocus = () => {
+    this.setState({ focusedInput: "END_DATE" });
+  };
+
+  format(date) {
+    if (!date) return "";
+    return date.toISOString();
+  }
+
+  render() {
+    return (
+      <div>
+        <Input
+          value={this.format(this.state.value.from)}
+          onFocus={this.handleStartInputFocus}
+          active={this.state.focusedInput === "START_DATE"}
+          readOnly
+        />
+        <Input
+          value={this.format(this.state.value.to)}
+          onFocus={this.handleEndInputFocus}
+          active={this.state.focusedInput === "END_DATE"}
+          readOnly
+        />
+        <DatePicker
+          focusedInput={this.state.focusedInput}
+          highlighted={this.state.highlighted}
+          onChange={this.handleChange}
+          onFocusedInputChange={this.handleFocusedInputChange}
+          onHighlightedChange={this.handleHighlightedChange}
+          selected={this.state.value}
+          sundayFirstDayOfWeek
+          viewDate={new Date(2017, 11, 1)}
+        />
+      </div>
+    );
+  }
+}
+
+export default PickerDemo;
---
+++ components/recompose.js
@@ -0,0 +1,43 @@
+import React from "react";
+import fmt from "date-fns/format";
+import { compose, withState } from "recompose";
+
+import Input from "./Input";
+import DatePicker from "./DatePicker";
+
+const END_DATE = "END_DATE";
+const START_DATE = "START_DATE";
+const format = d => (d ? fmt(d, "DD/MM/YYYY") : "");
+const enhance = compose(
+  withState("focused", "onFocus", null),
+  withState("highlighted", "onHighlight", { from: null, to: null }),
+  withState("value", "onChange", { from: null, to: null })
+);
+
+export default enhance(
+  ({ focused, highlighted, onChange, onFocus, onHighlight, value }) => (
+    <div>
+      <Input
+        value={format(value.from)}
+        onFocus={() => onFocus(START_DATE)}
+        active={focused === START_DATE}
+        readOnly
+      />
+      <Input
+        value={format(value.to)}
+        onFocus={() => onFocus(END_DATE)}
+        active={focused === END_DATE}
+        readOnly
+      />
+      <DatePicker
+        focusedInput={focused}
+        highlighted={highlighted}
+        onChange={onChange}
+        onFocusedInputChange={onFocus}
+        onHighlightedChange={onHighlight}
+        selected={value}
+        viewDate={new Date()}
+      />
+    </div>
+  )
+);
