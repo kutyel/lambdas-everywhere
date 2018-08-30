495a95269f2571bb43335f84bb41e6b064acdbe5
kutyel <flavio.corparios@ext.soprasteria.com>
2018-08-30 13:40:00 +0200

    Add react spinning logo... ⚛️

---
+++ components/react-logo.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 300px;
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`
